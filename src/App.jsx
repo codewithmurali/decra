9d387f342648b8492eda4c0ec64f0c1bd4363ad2
codewithmurali <muralikumar.admire@gmail.com>
2023-08-16 19:11:08 +0530

    making changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from "./Pages/About/About";
 import Events from "./Pages/Events/Events";
 import Contact from "./Pages/Contact/Contact";
 import Navbar from "./Components/Navbar/Navbar";
-import Landingpage from "./Components/Hero/landingpage";
+import Landingpage from "./Components/Hero/Landingpage";
 function App() {
   return (
     <div className="App">
