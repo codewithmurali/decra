1e93d6eb52769c0ad79cc030c84b3e2e2886eb73
codewithmurali <muralikumar.admire@gmail.com>
2023-08-21 19:22:32 +0530

    making changes for responsiveness

--- src/Components/Hero/Landingpage.jsx
+++ src/Components/Hero/Landingpage.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import hero from "../../assets/hero.mp4";
 import styles from "./landingpage.module.css";
+import Popup from "../Popup/Popup";
 
 const Landingpage = () => {
   return (
     <div className={`${styles.landingpage} `} id="home">
+      <Popup />
+
       <div className={styles.overlay}></div>
       <video src={hero} autoPlay muted loop className={styles.video_bg} />
       <div className={styles.content}>
         <h1 className={`${styles.gradient__text}`}>TEKCLUSTER'23</h1>
         <h2 className={`${styles.gradient__text} ${styles.sub__title}`}>ECE</h2>
-        <h4 className={`${styles.sub__para} `}>
-          {" "}
-          We value the interest of "Young Minds". So we have extended the paper
-          submission date till{" "}
-          <span className={styles.date}>AUGUST 21.08.2023</span>
-        </h4>
       </div>
     </div>
   );
--- src/Components/Popup/Popup.jsx
+++ src/Components/Popup/Popup.jsx
@@ -1,42 +1,8 @@
 import React from "react";
 
 const Popup = () => {
-  return (
-
-    const clickBtn = document.getElementById("clickBtn");
-    const popup = document.getElementById("popup");
-    const closeBtn = document.getElementById("closeBtn");
-    
-    clickBtn.addEventListener('click', ()=>{
-        popup.style.display = 'block';
-    });
-    closeBtn.addEventListener('click', ()=>{
-        popup.style.display = 'none';
-    });
-    popup.addEventListener('click', ()=>{
-        popup.style.display = 'none';
-    });
-
-  }
-    <div>
-      <div id="popup">
-        <div class="popup-container">
-          <div class="popup">
-            <div class="close-popup" id="closeBtn">
-              <a href="#">X</a>
-            </div>
-            <h2>Information</h2>
-            <p>
-              We value the interest of "Young Minds". So we have extended the
-              paper submission date till AUGUST 21.08.2023
-            </p>
-            <a href="#" class="popup-btn">
-              View Details
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+  return confirm(
+    `We value the interest of "Young Minds". So we have extended the paper submission date till AUGUST 21.08.2023`
   );
 };
 
