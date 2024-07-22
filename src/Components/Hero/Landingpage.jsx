cda25001b5e362d24fe0f10a126f2feba5b38416
MURALIKUMAR J <muralikumar.admire@gmail.com>
2024-07-22 13:06:51 +0530

    adding reg button

--- src/Components/Hero/Landingpage.jsx
+++ src/Components/Hero/Landingpage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import hero from "../../assets/hero.mp4";
 import styles from "./landingpage.module.css";
+import { Link } from "react-scroll";
 
 const Landingpage = () => {
   return (
@@ -10,6 +11,29 @@ const Landingpage = () => {
       <div className={styles.content}>
         <h1 className={`${styles.gradient__text}`}>TEKCLUSTER'23</h1>
         <h2 className={`${styles.gradient__text} ${styles.sub__title}`}>ECE</h2>
+
+        <div className={styles.submit}>
+          <Link to="events" spy={true} smooth={true} offset={50} duration={500}>
+            Register Here{" "}
+          </Link>
+          <div className={styles.icon}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 18"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-2"
+              height="16px"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 6 9 12.75l4.286-4.286a11.948 11.948 0 0 1 4.306 6.43l.776 2.898m0 0 3.182-5.511m-3.182 5.51-5.511-3.181"
+              />
+            </svg>
+          </div>
+        </div>
       </div>
     </div>
   );
